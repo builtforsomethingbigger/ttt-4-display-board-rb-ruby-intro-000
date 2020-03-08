12da658407eed54fdcceb313b8fa09eb0dce31b8
Steve C <stephen.cases@gmail.com>
2020-03-08 13:02:31 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,4 +6,4 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-display_board(["O", " ", " ", " ", " ", " ", " ", " ", " "])
+display_board(["O", "X", "X", "O", "X", "O", " ", " ", " "])
